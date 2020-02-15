f0917e31857d5b867bcdd85f6e22c5fd931573a8
root <federikoaugello@gmail.com>
2020-02-15 11:35:29 +0100

    Added comments to Section2-6

--- GetGoing/Section2/Section2-6/main.go
+++ GetGoing/Section2/Section2-6/main.go
@@ -2,39 +2,46 @@ package main
 
 import "fmt"
 
+// Car Interface with methods Drive and Stop
 type Car interface {
 	Drive()
 	Stop()
 }
 
+// Lambo struct
 type Lambo struct {
 	LamboModel string
 }
 
+// Chevy struct
 type Chevy struct {
 	ChevyModel string
 }
 
-// forces to declare all the methods of the interface to a specific structure
+// Forces to declare all the methods of the interface to a specific structure
 func newModel(arg string) Car {
 	return &Lambo{arg}
 }
 
+// Drive method for Lambo
 func (l *Lambo) Drive() {
 	fmt.Println("Lambo going...")
 	fmt.Println(l.LamboModel)
 }
 
+// Drive method for Chevy
 func (c *Chevy) Drive() {
 	fmt.Println("Chevy going...")
 	fmt.Println(c.ChevyModel)
 }
 
+// Stop method for Lambo
 func (l *Lambo) Stop() {
 	fmt.Println("Lambo stops...")
 	fmt.Println(l.LamboModel)
 }
 
+// Stop method for Chevy
 func (c *Chevy) Stop() {
 	fmt.Println("Chevy stops...")
 	fmt.Println(c.ChevyModel)
